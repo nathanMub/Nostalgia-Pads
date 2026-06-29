85a93a7751cc47d57c91fa3d13d84435cfc4ac41
nathan mubvuta <135868806+nathanMub@users.noreply.github.com>
2026-06-29 17:14:30 +0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from '@/context/CartContext';
+import { ThemeProvider } from '@/context/ThemeContext';
+
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
+import { CartDrawer } from '@/components/CartDrawer';
+
+import { HomePage } from '@/pages/HomePage';
+import { CollectionsPage } from '@/pages/CollectionsPage';
+import { CollectionPage } from '@/pages/CollectionPage';
+import { ProductPage } from '@/pages/ProductPage';
+import { CheckoutPage } from '@/pages/CheckoutPage';
+import { OrderSuccessPage } from '@/pages/OrderSuccessPage';
+
+import { Toaster } from 'sonner';
+import '@/App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <div className="App min-h-screen flex flex-col">
+            <Header />
+
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/collections" element={<CollectionsPage />} />
+                <Route path="/collection/:slug" element={<CollectionPage />} />
+                <Route path="/product/:slug" element={<ProductPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/order-success" element={<OrderSuccessPage />} />
+
+                {/* ✅ FIX: prevents blank 404 routes */}
+                <Route path="*" element={<HomePage />} />
+              </Routes>
+            </main>
+
+            <Footer />
+            <CartDrawer />
+            <Toaster position="top-right" richColors />
+          </div>
+        </BrowserRouter>
+      </CartProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
